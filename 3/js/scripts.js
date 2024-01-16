878d4d718453d3a89be70e8b9b6a7c3a64014699
Neha Sharma <nehasharma0321@gmail.com>
2024-01-16 16:14:22 +0100

    feat: TASK-303 Handle form submit
    - change to iife to make the functionality legible

--- 3/js/scripts.js
+++ 3/js/scripts.js
@@ -1,8 +1,19 @@
-document.getElementById("registerForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevents the default form submission behavior
+(
+    /**
+     * Handle form submit on button click
+     *
+     * Calls Validation for the form and allows submission only if all fields are valid
+     *
+     * @returns void
+     */
+    function handleSubmit()  {
+        document.getElementById("registerForm").addEventListener("submit", function(event) {
+            event.preventDefault(); // Prevents the default form submission behavior
 
-    let isFormValid = validateForm();
-    if (isFormValid === true) {
-        document.getElementById("registerForm").submit();
+            let isFormValid = validateForm();
+            if (isFormValid === true) {
+                document.getElementById("registerForm").submit();
+            }
+        });
     }
-});
+)()
